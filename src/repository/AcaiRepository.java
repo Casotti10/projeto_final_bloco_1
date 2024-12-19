8e7264e449bc68b1e11f1d6f1917d5760ff9d9a3
Lucas <lucascasotti1@gmail.com>
2024-12-19 14:39:30 -0300

    atualizaçoes

--- src/repository/AcaiRepository.java
+++ src/repository/AcaiRepository.java
@@ -1,7 +1,10 @@
 package repository;
 
-
-
 public interface AcaiRepository {
 	
+	public void procurarPorNumero();
+	public void listarTodas();
+	public void cadastrar();
+	public void atualizar();
+	public void deletar();	
 }
